5d543dcd77c285e181a2a8cbef515459e62e9df7
luchobalot <lnbalot02@gmail.com>
2025-05-19 09:48:29 -0300

    Usuarios - Interfaz e Implementacion de la Interfaz

---
+++ API_Peliculas/Repositorio/IRepositorio/IUsuarioRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using API_Peliculas.Modelos;
+using API_Peliculas.Modelos.Dtos;
+
+namespace API_Peliculas.Repositorio.IRepositorio
+{
+    public interface IUsuarioRepositorio
+    {
+        ICollection<Usuario> GetUsuarios();
+
+        Usuario GetUsuario(int usarioId); 
+
+        bool IsUniqueUser(string usuario);
+
+        Task<UsuarioLoginRespuestaDto> Login(UsuarioLoginDto usuarioLoginDto);
+
+        Task<UsuarioDatosDto> Registro(UsuarioRegistroDto usuarioRegistroDto);
+    }
+}
---
+++ API_Peliculas/Repositorio/UsuarioRepositorio.cs
@@ -0,0 +1,48 @@
+﻿using API_Peliculas.Data;
+using API_Peliculas.Modelos;
+using API_Peliculas.Modelos.Dtos;
+using API_Peliculas.Repositorio.IRepositorio;
+
+namespace API_Peliculas.Repositorio
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly ApplicationDbContext _bd;
+
+        public UsuarioRepositorio(ApplicationDbContext bd)
+        {
+            _bd = bd;
+        }
+
+        public Usuario GetUsuario(int usarioId)
+        {
+            return _bd.Usuario.FirstOrDefault(c => c.Id == usarioId);
+        }
+
+        public ICollection<Usuario> GetUsuarios()
+        {
+            return _bd.Usuario.OrderBy(c => c.NombreUsuario).ToList();
+        }
+
+        public bool IsUniqueUser(string usuario)
+        {
+            var usuarioBd = _bd.Usuario.FirstOrDefault(u => u.NombreUsuario == usuario);
+
+            if (usuarioBd == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Task<UsuarioLoginRespuestaDto> Login(UsuarioLoginDto usuarioLoginDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UsuarioDatosDto> Registro(UsuarioRegistroDto usuarioRegistroDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
